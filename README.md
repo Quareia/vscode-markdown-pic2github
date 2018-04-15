74bc10e3932100acb438d11164319cf0e7e244f4
quareia <garyzhang195@gmail.com>
2018-04-15 22:48:24 +0800

    release v1.0.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# markdown-pic2github README
+![](https://img.shields.io/github/license/Quareia/vscode-markdown-pic2github.svg)
+
+1个vscode插件，提供markdown的github图片外链。编写markdown文件时可以上传本地图片并拷贝到本地git仓库，最后自动提交到github
+
+### Usage
+- settings.json中修改对应的属性
+    1. `local_git_folder`：本地存储图片的git仓库文件夹名，为绝对路径
+    2. `remote_github_repo`：github对应的图片仓库
+- `ctrl+alt+g`：选择上传图片
---
+++ extension.js
@@ -0,0 +1,73 @@
+// The module 'vscode' contains the VS Code extensibility API
+const vscode = require('vscode')
+const path = require('path')
+var exec = require('child_process').exec
+
+const error = err => {
+    vscode.window.showErrorMessage(err)
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.mdpic2github', function () {
+        
+        let editor = vscode.window.activeTextEditor
+        
+        // No open  file
+        if (!editor) {
+            vscode.window.showInformationMessage('No open file')
+            return
+        }
+        // Must be markdown file
+        let doc = editor.document
+        if (!(doc.languageId === "markdown")) {
+            vscode.window.showInformationMessage('Must be a markdown file')
+            return
+        }
+        let localFolder = vscode.workspace.getConfiguration('pic2github').get('local_git_folder')
+        let remoteRepo = vscode.workspace.getConfiguration('pic2github').get('remote_github_repo')
+       
+        vscode.window.showOpenDialog({
+            filters: { 'Images': ['png', 'jpg', 'gif', 'bmp'] }
+        }).then(result => {
+            if (result) {
+                // 解构
+                let {fsPath} = result[0]
+                let imgName = fsPath.split(path.sep).pop()
+                let cmdStr = 'cp ' + fsPath + ' ' + localFolder + ' && ' + 
+                'cd ' + localFolder + ' && ' +
+                'git add ' + imgName + ' && ' +
+                'git commit -m ' + doc.fileName.split(path.sep).pop() + ' && ' +
+                'git push'
+                console.log(cmdStr)
+                exec(cmdStr, function(err, stdout, stderr) {
+                    if (err) {
+                        console.log('wrong:' + stderr)
+                    } else {
+                        let data = stdout
+                        console.log(data)
+                    }
+                })
+                let imgUrl = '![](https://raw.githubusercontent.com/' + remoteRepo +'/master/'+ imgName + ')'
+                editor.edit(textEditorEdit => {
+                    textEditorEdit.insert(editor.selection.active, imgUrl)
+                })
+            }
+        }, error)
+        
+    });
+        
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
